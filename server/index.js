4e6cff6aa30f831231bcbb0006098f4b29266b4b
Ivan Klaric <all.online.regs@gmail.com>
2021-05-04 18:48:32 +0200

    .env ignore

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import cors from 'cors'
 import postRoutes from './routes/posts.js'
 
+
 const app = express();
 
 app.use(express.urlencoded({extended: true})); 
@@ -11,6 +12,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
+// const CONNECTION_URL = MONGO_URI
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
