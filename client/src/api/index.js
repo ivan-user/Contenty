7f5fdbdac8dd7bc98839fefdf2b30f0abb6e9f84
Ivan Klaric <all.online.regs@gmail.com>
2021-05-25 17:11:44 +0200

    Finally deployed and working

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'https://contenty-app.herokuapp.com/posts';
+const url = 'https://conteny-project.herokuapp.com/posts';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
--- server/index.js
+++ server/index.js
@@ -7,9 +7,9 @@ import postRoutes from './routes/posts.js'
 const app = express();
 dotenv.config();
 
+app.use(cors());
 app.use(express.urlencoded({extended: true})); 
 app.use(express.json());
-app.use(cors());
 
 app.get('/', (req, res) => {
     res.send("Hello to Contenty App");
